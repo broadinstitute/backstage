b4f5d546e7c13404dfa753e57bb82a3781b9fe8c
Alaric Hartmann <tfhartmann@gmail.com>
2024-01-30 12:35:30 -0500

    feat: Create secrets to Google Secret Manager
    Creating the empty secrets in Secret Manager so that we can populate them with the correct values for the app to use

--- deployment/dev/terraform/main.tf
+++ deployment/dev/terraform/main.tf
@@ -17,19 +17,53 @@ resource "google_project_service" "api_services" {
   disable_on_destroy         = false
 }
 
+locals {
+  google_secret_manager_secrets = {
+    github-app-backstage-bits-credentials = {
+      created-by       = "terraform"
+      terraform-module = "terraform-bits-${local.application_name}-${var.env}"
+      service          = "github"
+      type             = "yaml-file"
+    },
+    auth-github-client-id = {
+      created-by       = "terraform"
+      terraform-module = "terraform-bits-${local.application_name}-${var.env}"
+      service          = "github"
+      type             = "string"
+    },
+    auth-github-client-secret = {
+      created-by       = "terraform"
+      terraform-module = "terraform-bits-${local.application_name}-${var.env}"
+      service          = "github"
+      type             = "string"
+    },
+    auth-google-client-id = {
+      created-by       = "terraform"
+      terraform-module = "terraform-bits-${local.application_name}-${var.env}"
+      service          = "google"
+      type             = "string"
+    },
+    auth-google-client-secret = {
+      created-by       = "terraform"
+      terraform-module = "terraform-bits-${local.application_name}-${var.env}"
+      service          = "google"
+      type             = "string"
+    },
+  }
+}
 # Terraform creates this secret but does not manage it
-resource "google_secret_manager_secret" "github_app_credentials" {
+resource "google_secret_manager_secret" "backstage-bits-credentials" {
+  for_each  = local.google_secret_manager_secrets
   project   = var.core_project
-  secret_id = "github-app-backstage-bits-credentials"
+  secret_id = each.key
+  labels    = each.value
 
   replication {
     auto {}
   }
-
-  labels = {
-    created-by       = "terraform"
-    terraform-module = "terraform-bits-${local.application_name}-${var.env}"
-    service          = "github"
-    type             = "yaml-file"
-  }
 }
+
+#AUTH_GOOGLE_CLIENT_ID
+#AUTH_GOOGLE_CLIENT_SECRET
+#AUTH_GITHUB_CLIENT_ID
+#AUTH_GITHUB_CLIENT_SECRET
