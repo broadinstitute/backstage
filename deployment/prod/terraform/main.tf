9e0b2665abd07badfcf75ab1028eeb5d11e49fc3
Alaric Hartmann <tfhartmann@gmail.com>
2024-02-10 10:52:42 -0500

    feat: Update techdocs publisher to use Google Cloud Storage
    Adds both the backstage configs and the terraform to create and manage the bucket

--- deployment/prod/terraform/main.tf
+++ deployment/prod/terraform/main.tf
@@ -34,3 +34,30 @@ resource "google_secret_manager_secret" "backstage-bits-credentials" {
     auto {}
   }
 }
+
+resource "google_storage_bucket" "tech-docs" {
+  name                        = "${var.core_project}-tech-docs"
+  project                     = var.core_project
+  location                    = "US"
+  storage_class               = "STANDARD"
+  force_destroy               = false
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age                = 365
+      num_newer_versions = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
