0156d52d8024b77ee02bda3354b007adcd11361e
Alaric Hartmann <tfhartmann@gmail.com>
2024-02-23 09:52:12 -0500

    feat: Add Terraform control module files
    Add the skel and templates to generate a Terraform Repo using sofware templating.  I'm adding the files to the Container image, but at some point we should stop doing that, since it'll make the image even larger than it already is

---
+++ backstage/templates/scaffolder/terraform-control-module/content/dev/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "${{ values.terraformVersion }}"
+  backend "gcs" {
+    bucket = "${{ values.gcsBucket }}"
+    prefix = "${{ values.name }}/dev"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "${{ values.googleProviderVersion }}"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "${{ values.googleProviderVersion }}"
+    }
+  }
+}
