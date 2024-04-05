576ed38cb1e1ed2b48067e9fa376cae7fa781ed7
Alaric Hartmann <tfhartmann@gmail.com>
2024-04-05 16:47:16 -0400

    fix: update terraform templates to fix missing files and dirs (#30)
    * fix: update terraform templates to fix missing files and dirs
    
    Also added the enabled api services to the control repo template to that it would match more closely to the terragrunt template
    
    * fix: Add api_services variable to dev/variables.tf
    
    missed a file in the last commit
    
    * fix: Add missing locals.tf file for terraform control module
    
    * fix: Update project names in atlantis.yaml

--- backstage/templates/scaffolder/terraform-control-module/content/dev/variables.tf
+++ backstage/templates/scaffolder/terraform-control-module/content/dev/variables.tf
@@ -1,6 +1,14 @@
 # Input variables to accept values from the calling module
 # https://www.terraform.io/docs/modules/#standard-module-structure
 
+variable "api_services" {
+  description = "List of API services to enable"
+  type        = list(string)
+  default = [
+    "compute.googleapis.com",
+  ]
+}
+
 variable "core_project" {
   description = "GCP project managed by the default/primary provider"
   type        = string
