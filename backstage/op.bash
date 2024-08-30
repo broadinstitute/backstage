ca68aee2e71caa29680db2a9e4ddbc7f0092fbc1
Alaric Hartmann <tfhartmann@gmail.com>
2024-08-30 09:40:11 -0400

    chore: Update docs, and envrc
    This should provide more information about how to develop for backstage locally, provide some automation for team members to get secrets, and should make getting started easier.

---
+++ backstage/op.bash
@@ -0,0 +1,15 @@
+#!/bin/env bash
+
+# Google
+AUTH_GOOGLE_CLIENT_ID=$(op --account broadinstitute.1password.com read "op://BITS - Backstage/OAuth 2.0 Client ID - Dev/client_id")
+export AUTH_GOOGLE_CLIENT_ID
+AUTH_GOOGLE_CLIENT_SECRET=$(op --account broadinstitute.1password.com read "op://BITS - Backstage/OAuth 2.0 Client ID - Dev/client_secret")
+export AUTH_GOOGLE_CLIENT_SECRET
+# GitHub
+AUTH_GITHUB_CLIENT_ID=$(op --account broadinstitute.1password.com read "op://BITS - Backstage/auth-github-client-id-dev/credential")
+export AUTH_GITHUB_CLIENT_ID
+AUTH_GITHUB_CLIENT_SECRET=$(op --account broadinstitute.1password.com read "op://BITS - Backstage/auth-github-client-secret-dev/credential")
+export AUTH_GITHUB_CLIENT_SECRET
+op --account broadinstitute.1password.com read --out-file ./github-app-backstage-bits-dev-credentials.yaml "op://BITS - Backstage/github-app-backstage-bits-dev-credentials/github-app-backstage-bits-dev-credentials.yaml"
+
+# vim: set et fenc=utf-8 ff=unix ft=sh sts=2 sw=2 ts=2 :
