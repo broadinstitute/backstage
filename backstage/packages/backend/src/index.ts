77cf7dc8dac536fcfbf83fdd7d248fa886a2d32b
Alaric Hartmann <tfhartmann@gmail.com>
2024-09-19 10:09:26 -0400

    feat: Migrated to the new backend
    The new backend system simplified the backstage configuration, in particular plugins.  The impact of this is a cleaner app, that easier for us to manage and reason about.
    
    More info can be found here https://backstage.io/docs/backend-system/building-backends/migrating/

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -1,7 +1,24 @@
 import { createBackend } from '@backstage/backend-defaults';
 import { createBackendModule } from '@backstage/backend-plugin-api';
 import { githubOrgEntityProviderTransformsExtensionPoint } from '@backstage/plugin-catalog-backend-module-github-org';
+// Import { legacyPlugin } from '@backstage/backend-common';
+import { myVerifiedUserTransformer } from './transformers';
 
+// Transforms the User entity to add the verified email from the GitHub Org
+const githubOrgModule = createBackendModule({
+    pluginId: 'catalog',
+    moduleId: 'github-org-extensions',
+    register(env) {
+        env.registerInit({
+            deps: {
+                githubOrg: githubOrgEntityProviderTransformsExtensionPoint,
+            },
+            async init({ githubOrg }) {
+                githubOrg.setUserTransformer(myVerifiedUserTransformer);
+            },
+        });
+    },
+});
 
 const backend = createBackend();
 
@@ -24,7 +41,7 @@ backend.add(import('@backstage/plugin-catalog-backend/alpha'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 backend.add(
-  import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
+    import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
@@ -34,7 +51,7 @@ backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
 // See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
 backend.add(
-  import('@backstage/plugin-permission-backend-module-allow-all-policy'),
+    import('@backstage/plugin-permission-backend-module-allow-all-policy'),
 );
 
 // search plugin
@@ -51,127 +68,7 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
+// Map  Org emails to GitHub user entities in the catalog
+backend.add(githubOrgModule);
+
 backend.start();
-
-
-// /*
-//  * Hi!
-//  *
-//  * Note that this is an EXAMPLE Backstage backend. Please check the README.
-//  *
-//  * Happy hacking!
-//  */
-
-// import Router from 'express-promise-router';
-// import {
-//   createServiceBuilder,
-//   loadBackendConfig,
-//   getRootLogger,
-//   useHotMemoize,
-//   notFoundHandler,
-//   CacheManager,
-//   DatabaseManager,
-//   HostDiscovery,
-//   UrlReaders,
-//   ServerTokenManager,
-// } from '@backstage/backend-common';
-// import { TaskScheduler } from '@backstage/backend-tasks';
-// import { Config } from '@backstage/config';
-// import app from './plugins/app';
-// import auth from './plugins/auth';
-// import catalog from './plugins/catalog';
-// import scaffolder from './plugins/scaffolder';
-// import proxy from './plugins/proxy';
-// import techdocs from './plugins/techdocs';
-// import search from './plugins/search';
-// import { PluginEnvironment } from './types';
-// import { ServerPermissionClient } from '@backstage/plugin-permission-node';
-// import { DefaultIdentityClient } from '@backstage/plugin-auth-node';
-// import kubernetes from './plugins/kubernetes';
-// import pagerduty from './plugins/pagerduty';
-
-// function makeCreateEnv(config: Config) {
-//   const root = getRootLogger();
-//   const reader = UrlReaders.default({ logger: root, config });
-//   const discovery = HostDiscovery.fromConfig(config);
-//   const cacheManager = CacheManager.fromConfig(config);
-//   const databaseManager = DatabaseManager.fromConfig(config, { logger: root });
-//   const tokenManager = ServerTokenManager.noop();
-//   const taskScheduler = TaskScheduler.fromConfig(config, { databaseManager });
-
-//   const identity = DefaultIdentityClient.create({
-//     discovery,
-//   });
-//   const permissions = ServerPermissionClient.fromConfig(config, {
-//     discovery,
-//     tokenManager,
-//   });
-
-//   root.info(`Created UrlReader ${reader}`);
-
-//   return (plugin: string): PluginEnvironment => {
-//     const logger = root.child({ type: 'plugin', plugin });
-//     const database = databaseManager.forPlugin(plugin);
-//     const cache = cacheManager.forPlugin(plugin);
-//     const scheduler = taskScheduler.forPlugin(plugin);
-//     return {
-//       logger,
-//       database,
-//       cache,
-//       config,
-//       reader,
-//       discovery,
-//       tokenManager,
-//       scheduler,
-//       permissions,
-//       identity,
-//     };
-//   };
-// }
-
-// async function main() {
-//   const config = await loadBackendConfig({
-//     argv: process.argv,
-//     logger: getRootLogger(),
-//   });
-//   const createEnv = makeCreateEnv(config);
-
-//   const catalogEnv = useHotMemoize(module, () => createEnv('catalog'));
-//   const scaffolderEnv = useHotMemoize(module, () => createEnv('scaffolder'));
-//   const authEnv = useHotMemoize(module, () => createEnv('auth'));
-//   const proxyEnv = useHotMemoize(module, () => createEnv('proxy'));
-//   const techdocsEnv = useHotMemoize(module, () => createEnv('techdocs'));
-//   const searchEnv = useHotMemoize(module, () => createEnv('search'));
-//   const appEnv = useHotMemoize(module, () => createEnv('app'));
-//   const kubernetesEnv = useHotMemoize(module, () => createEnv('kubernetes'));
-//   const pagerdutyEnv = useHotMemoize(module, () => createEnv('pagerduty'));
-
-//   const apiRouter = Router();
-//   apiRouter.use('/catalog', await catalog(catalogEnv));
-//   apiRouter.use('/scaffolder', await scaffolder(scaffolderEnv));
-//   apiRouter.use('/auth', await auth(authEnv));
-//   apiRouter.use('/techdocs', await techdocs(techdocsEnv));
-//   apiRouter.use('/proxy', await proxy(proxyEnv));
-//   apiRouter.use('/search', await search(searchEnv));
-//   apiRouter.use('/kubernetes', await kubernetes(kubernetesEnv));
-//   apiRouter.use('/pagerduty', await pagerduty(pagerdutyEnv));
-
-//   // Add backends ABOVE this line; this 404 handler is the catch-all fallback
-//   apiRouter.use(notFoundHandler());
-
-//   const service = createServiceBuilder(module)
-//     .loadConfig(config)
-//     .addRouter('/api', apiRouter)
-//     .addRouter('', await app(appEnv));
-
-//   await service.start().catch(err => {
-//     console.log(err);
-//     process.exit(1);
-//   });
-// }
-
-// module.hot?.accept();
-// main().catch(error => {
-//   console.error('Backend failed to start up', error);
-//   process.exit(1);
-// });
