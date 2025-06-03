6e9d1075935c2974a4d5017df9165a710915bb08
Andrew Teixeira <teixeira@broadinstitute.org>
2025-06-03 17:15:37 -0400

    prod

--- backstage/templates/scaffolder/terraform-mediawiki/content/prod/terraform.tf
+++ backstage/templates/scaffolder/terraform-mediawiki/content/prod/terraform.tf
@@ -1,9 +1,5 @@
 terraform {
   required_version = "${{ values.terraformVersion }}"
-  backend "gcs" {
-    bucket = "${{ values.gcsBucket }}"
-    prefix = "${{ values.name }}/state"
-  }
 
   required_providers {
     google = {
@@ -14,8 +10,12 @@ terraform {
       source  = "hashicorp/google-beta"
       version = "${{ values.googleProviderVersion }}"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
     time = {
-      source = "hashicorp/time"
+      source  = "hashicorp/time"
       version = "0.13.1"
     }
   }
