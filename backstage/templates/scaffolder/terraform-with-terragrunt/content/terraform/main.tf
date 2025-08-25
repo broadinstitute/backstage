1c05ee1c6468fb193a82f5a03c7ecaf1b84fba76
Andrew Teixeira <teixeira@broadinstitute.org>
2025-08-25 17:04:55 -0400

    feat: Update terraform-with-terragrunt template

--- backstage/templates/scaffolder/terraform-with-terragrunt/content/terraform/main.tf
+++ backstage/templates/scaffolder/terraform-with-terragrunt/content/terraform/main.tf
@@ -3,8 +3,3 @@ data "google_client_config" "current" {}
 data "google_project" "current" {
   project_id = var.core_project
 }
-
-provider "google" {
-  project = var.core_project
-  region  = "${{ values.gcpRegion }}"
-}
