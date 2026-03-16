b05f657986670bf6a68959b31827834b87a3c295
Alaric Hartmann <tfhartmann@gmail.com>
2026-03-16 13:27:36 -0400

    feat: Upgrade backstage to 1.48.5
    Keeping Core app updated

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -148,7 +148,7 @@ backend.add(import('@backstage/plugin-search-backend-module-pg'));
 
 // search collators
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
-backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
+//backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
