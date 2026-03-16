cc958b6b7305bba7859d2632af2c343f8818bb24
Alaric Hartmann <tfhartmann@gmail.com>
2026-03-16 14:22:56 -0400

    fix: remove unused techdocs search backend module import
    This module broke during upgrade and looks like it's no longer needed

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -148,7 +148,6 @@ backend.add(import('@backstage/plugin-search-backend-module-pg'));
 
 // search collators
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
-//backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
