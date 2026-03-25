90654c7986e3f4545a2239ba9a1068535fb9d18f
Alaric Hartmann <tfhartmann@gmail.com>
2026-03-25 11:45:03 -0400

    fix(terraform): update Google provider version to 7.25.0 and PostgreSQL provider version to 1.25.0 in both dev and prod environments

--- deployment/dev/terraform/terraform.tf
+++ deployment/dev/terraform/terraform.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "7.12.0"
+      version = "7.25.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "7.12.0"
+      version = "7.25.0"
     }
     postgresql = {
       source  = "cyrilgdn/postgresql"
